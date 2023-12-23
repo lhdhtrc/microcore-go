190bc4a6ec453d4214970706916610db79dd388b
lhdht <15229684090@163.com>
2023-12-23 14:13:22 +0800

    feature: "micro service model"

---
+++ micro/model.go
@@ -0,0 +1,20 @@
+package micro
+
+type ConfigEntity struct {
+	Address   string              `json:"address" yaml:"address" mapstructure:"address"`       // current service deploy address outside
+	Namespace string              `json:"namespace" yaml:"namespace" mapstructure:"namespace"` // current service namespace format service/group/xxxx
+	Service   map[string][]string `json:"service" yaml:"service" mapstructure:"service"`       // service discover watch service
+	MaxRetry  uint32              `json:"max_retry" yaml:"maxRetry" mapstructure:"maxRetry"`   // service global lease unexpected disconnection retry max frequency
+	Deploy    bool                `json:"deploy" yaml:"maxRetry" mapstructure:"maxRetry"`      // current service deploy mode: local area network or not
+}
+
+type ValueEntity struct {
+	Name      string `json:"name"`
+	Endpoints string `json:"endpoints"`
+}
+
+type DiscoverEntity struct {
+	Gateway   string `json:"gateway"`
+	Namespace string `json:"namespace"`
+	Service   string `json:"service"`
+}

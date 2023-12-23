02b175b9dac39493ba34ca0d951844b01391ecd5
lhdht <15229684090@163.com>
2023-12-23 14:19:51 +0800

    feature: "db base model"

---
+++ model/base/db.model.go
@@ -0,0 +1,26 @@
+package base
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+	"time"
+)
+
+type DBTableEntity struct {
+	ID        uint           `json:"id" gorm:"primarykey"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+}
+
+type DBTableUUIDEntity struct {
+	ID        string         `json:"id" gorm:"primarykey;"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
+}
+
+func (s *DBTableUUIDEntity) BeforeCreate(_ *gorm.DB) (err error) {
+	s.ID = uuid.New().String()
+	return
+}

107b19ad06f9ad7c790d219f2d8e8844e63b637e
lhdht <15229684090@163.com>
2023-12-27 14:09:54 +0800

    perfect: "抽离logger配置"

--- logger/entrance.go
+++ logger/entrance.go
@@ -9,15 +9,16 @@ type Abstraction interface {
 	Mongo(log string)
 }
 
+type ConfigEntity struct {
+	Console   bool `json:"console" bson:"console" yaml:"console" mapstructure:"console"`             // Console whether to output logs on the console
+	Enable    bool `json:"enable" bson:"enable" yaml:"enable" mapstructure:"enable"`                 // Enable whether to enable logs
+	UseRemote bool `json:"use_remote" bson:"use_remote" yaml:"use_remote" mapstructure:"use_remote"` // UseRemote whether to remote logs
+}
+
 type EntranceEntity struct {
-	Temp [][]string // Temp storage logs
-
-	Console bool // Console whether to output logs on the console
-	Enable  bool // Enable whether to enable logs
-
-	Remote    func(level string, message string) // Remote storage logs func
-	UseRemote bool                               // UseRemote whether to remote logs
-
+	Config ConfigEntity                       // Config logger configs
+	Temp   [][]string                         // Temp storage logs
+	Remote func(level string, message string) // Remote storage logs func
 }
 
 func Use(config *EntranceEntity) Abstraction {
--- logger/internal/entrance.go
+++ logger/internal/entrance.go
@@ -6,18 +6,18 @@ import (
 	"time"
 )
 
-func Logger(config *logger.EntranceEntity, level string, message string) {
-	if config.Enable {
-		if config.Console {
+func Logger(options *logger.EntranceEntity, level string, message string) {
+	if options.Config.Enable {
+		if options.Config.Console {
 			fmt.Printf("%s %s %s", time.Now().Format("2006-01-02 15:04:05"), level, message)
 		}
 
-		if config.UseRemote {
-			if config.Remote != nil {
-				config.Remote(level, message)
+		if options.Config.UseRemote {
+			if options.Remote != nil {
+				options.Remote(level, message)
 			} else {
 				item := []string{level, message}
-				config.Temp = append(config.Temp, item)
+				options.Temp = append(options.Temp, item)
 			}
 		}
 	}

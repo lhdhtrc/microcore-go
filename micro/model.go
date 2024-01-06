a705bb80975303cea6a53843424574c3620e454c
lhdht <15229684090@163.com>
2024-01-06 14:59:31 +0800

    fix: "废弃无效字段"

--- micro/model.go
+++ micro/model.go
@@ -4,7 +4,6 @@ type ConfigEntity struct {
 	Address   string `json:"address" bson:"address" yaml:"address" mapstructure:"address"`         // current service deploy address outside
 	Namespace string `json:"namespace" bson:"namespace" yaml:"namespace" mapstructure:"namespace"` // current service namespace format service/group/xxxx
 	MaxRetry  uint32 `json:"max_retry" bson:"max_retry" yaml:"max_retry" mapstructure:"max_retry"` // service global lease unexpected disconnection retry max frequency
-	Deploy    bool   `json:"deploy" bson:"deploy" yaml:"deploy" mapstructure:"deploy"`             // current service deploy mode: local area network or not
 	TTL       uint32 `json:"ttl" bson:"ttl" yaml:"ttl" mapstructure:"ttl"`                         // current service lease time
 }
 

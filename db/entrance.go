42ae061253179f4d3372a63e9dfb32768d8a2047
lhdht <15229684090@163.com>
2024-01-28 20:22:46 +0800

    fix: db config field map

--- db/entrance.go
+++ db/entrance.go
@@ -17,11 +17,11 @@ type ConfigEntity struct {
 	Mode bool `json:"mode" yaml:"mode" mapstructure:"mode"` // Mode is true cluster
 	Auth uint `json:"auth" yaml:"auth" mapstructure:"auth"` // Auth way, account+password / TLS
 
-	MaxOpenConnects        int  `json:"max_open_connect" bson:"max_open_connect" yaml:"max_open_connect" mapstructure:"max_open_connect"`
-	MaxIdleConnects        int  `json:"max_idle_connect" bson:"max_idle_connect" yaml:"max_idle_connect" mapstructure:"max_idle_connect"`
+	MaxOpenConnects        int  `json:"max_open_connects" bson:"max_open_connects" yaml:"max_open_connects" mapstructure:"max_open_connects"`
+	MaxIdleConnects        int  `json:"max_idle_connects" bson:"max_idle_connects" yaml:"max_idle_connects" mapstructure:"max_idle_connects"`
 	ConnMaxLifeTime        int  `json:"conn_max_life_time" bson:"conn_max_life_time" yaml:"conn_max_life_time" mapstructure:"conn_max_life_time"`
 	SkipDefaultTransaction bool `json:"skip_default_transaction" bson:"skip_default_transaction" yaml:"skip_default_transaction" mapstructure:"skip_default_transaction"`
-	PrepareStmt            bool `json:"prepare_stmt" bson:"prepareStmt" yaml:"prepare_stmt" mapstructure:"prepare_stmt"`
+	PrepareStmt            bool `json:"prepare_stmt" bson:"prepare_stmt" yaml:"prepare_stmt" mapstructure:"prepare_stmt"`
 
 	LoggerEnable bool `json:"logger_enable" bson:"logger_enable" yaml:"logger_enable" mapstructure:"logger_enable"`
 }

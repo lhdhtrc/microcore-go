72cddf9227fedc4351d61f7482c23e68cb9f0aad
lhdht <15229684090@163.com>
2024-02-23 16:34:01 +0800

    fix: 修复删除时一直有数据的问题

--- utils/gorm/tree.go
+++ utils/gorm/tree.go
@@ -40,7 +40,7 @@ func Tree[T interface{}](options TreeOptions) ([]*T, error) {
 			parentId = fmt.Sprintf(" AND parent_id = '%s'", options.Id)
 		}
 
-		statement := fmt.Sprintf(`WITH RECURSIVE tree AS (SELECT * FROM %s WHERE app_id = '%s' %s UNION ALL SELECT t.* FROM %s t INNER JOIN tree ON tree.id = t.parent_id) SELECT * FROM tree;`, options.Table, options.AppId, parentId, options.Table)
+		statement := fmt.Sprintf(`WITH RECURSIVE tree AS (SELECT * FROM %s WHERE app_id = '%s' %s AND deleted_at IS NULL UNION ALL SELECT t.* FROM %s t INNER JOIN tree ON tree.id = t.parent_id) SELECT * FROM tree;`, options.Table, options.AppId, parentId, options.Table)
 		sql = options.DB.Raw(statement)
 		for _, field := range options.Preload {
 			sql.Preload(field)

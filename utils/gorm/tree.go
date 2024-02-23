5e36f94f93f09153823e7b131cb8d65231fb70ed
lhdht <15229684090@163.com>
2024-02-23 14:03:08 +0800

    perfect: "改造支持懒加载"

--- utils/gorm/tree.go
+++ utils/gorm/tree.go
@@ -12,32 +12,45 @@ type TreeOptions struct {
 	AppId   string
 	Id      string
 	Preload []string
+	Lazy    bool
 }
 
-func Tree[T interface{}](options TreeOptions) (T, []*T, error) {
-	parentId := options.Id
+func Tree[T interface{}](options TreeOptions) ([]*T, error) {
+	var (
+		row  T
+		list []*T
+		sql  *gorm.DB
+	)
 
-	if options.Id == "" {
-		options.Id = "IS NULL"
-	} else {
-		options.Id = fmt.Sprintf("= '%s'", options.Id)
-	}
-
-	statement := fmt.Sprintf(`WITH RECURSIVE tree AS (SELECT * FROM %s WHERE app_id = '%s' AND parent_id %s UNION ALL SELECT t.* FROM %s t INNER JOIN tree ON tree.id = t.parent_id) SELECT * FROM tree;`, options.Table, options.AppId, options.Id, options.Table)
-
-	var sql *gorm.DB
-	var row T
-	var list []*T
-	sql = options.DB.Raw(statement)
-	if options.Id != "IS NULL" {
-		if res := options.DB.Where("id = ?", parentId).First(&row); errors.Is(res.Error, gorm.ErrRecordNotFound) {
-			return row, list, res.Error
-		}
-	}
+	sql = options.DB.Table(options.Table)
 	for _, field := range options.Preload {
 		sql.Preload(field)
 	}
+	sql.Where("app_id = ?", options.AppId)
+	if options.Id != "" {
+		sql.Where("id = ?", options.Id)
+		if res := sql.First(&row); errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			return list, res.Error
+		}
+	}
+
+	if !options.Lazy {
+		var parentId string
+		if options.Id != "" {
+			parentId = fmt.Sprintf(" AND parent_id = '%s'", options.Id)
+		}
+
+		statement := fmt.Sprintf(`WITH RECURSIVE tree AS (SELECT * FROM %s WHERE app_id = '%s' %s UNION ALL SELECT t.* FROM %s t INNER JOIN tree ON tree.id = t.parent_id) SELECT * FROM tree;`, options.Table, options.AppId, parentId, options.Table)
+		sql = options.DB.Raw(statement)
+		for _, field := range options.Preload {
+			sql.Preload(field)
+		}
+	}
 	sql.Find(&list)
 
-	return row, list, nil
+	if len(options.Id) != 0 {
+		list = append(list, &row)
+	}
+
+	return list, nil
 }

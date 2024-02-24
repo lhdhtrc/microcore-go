eb3f12fc957af3296ecc1098836ed2d299a47472
lhdht <15229684090@163.com>
2024-02-24 18:42:34 +0800

    perfect: 使用sort进行排序

--- utils/gorm/tree.go
+++ utils/gorm/tree.go
@@ -27,6 +27,7 @@ func Tree[T interface{}](options TreeOptions) ([]*T, error) {
 		sql.Preload(field)
 	}
 	sql.Where("app_id = ?", options.AppId)
+	sql.Order("sort DESC")
 	if options.Id != "" {
 		sql.Where("id = ?", options.Id)
 		if res := sql.First(&row); errors.Is(res.Error, gorm.ErrRecordNotFound) {
@@ -40,7 +41,7 @@ func Tree[T interface{}](options TreeOptions) ([]*T, error) {
 			parentId = fmt.Sprintf(" AND parent_id = '%s'", options.Id)
 		}
 
-		statement := fmt.Sprintf(`WITH RECURSIVE tree AS (SELECT * FROM %s WHERE app_id = '%s' %s AND deleted_at IS NULL UNION ALL SELECT t.* FROM %s t INNER JOIN tree ON tree.id = t.parent_id) SELECT * FROM tree;`, options.Table, options.AppId, parentId, options.Table)
+		statement := fmt.Sprintf(`WITH RECURSIVE tree AS (SELECT * FROM %s WHERE app_id = '%s' %s AND deleted_at IS NULL UNION ALL SELECT t.* FROM %s t INNER JOIN tree ON tree.id = t.parent_id) SELECT * FROM tree ORDER BY sort DESC;`, options.Table, options.AppId, parentId, options.Table)
 		sql = options.DB.Raw(statement)
 		for _, field := range options.Preload {
 			sql.Preload(field)

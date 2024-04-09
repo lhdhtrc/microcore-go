5924a67f75df8bf2c0abd838d8ea3b48a76c0588
lhdht <15229684090@163.com>
2024-04-09 10:54:45 +0800

    fix: mongo实体类

---
+++ model/base/mongo.model.go
@@ -0,0 +1,13 @@
+package base
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type MongoTableModel struct {
+	ID        primitive.ObjectID `json:"id" bson:"_id"`
+	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
+	DeletedAt time.Time          `json:"deleted_at" bson:"deleted_at"`
+}

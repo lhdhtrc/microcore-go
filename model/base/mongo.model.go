e2156be90d1b32158fe0d2d509980070c12bb8bf
lhdht <15229684090@163.com>
2024-04-09 21:24:23 +0800

    feature: 钩子函数

--- model/base/mongo.model.go
+++ model/base/mongo.model.go
@@ -11,3 +11,18 @@ type MongoTableModel struct {
 	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
 	DeletedAt time.Time          `json:"deleted_at" bson:"deleted_at"`
 }
+
+func (mm *MongoTableModel) BeforeInset() {
+	mm.ID = primitive.NewObjectID()
+	mm.CreatedAt = time.Now().Local()
+	mm.UpdatedAt = time.Now().Local()
+}
+
+func (mm *MongoTableModel) BeforeUpdate() {
+	mm.UpdatedAt = time.Now().Local()
+}
+
+func (mm *MongoTableModel) BeforeDelete() {
+	mm.UpdatedAt = time.Now().Local()
+	mm.DeletedAt = time.Now().Local()
+}

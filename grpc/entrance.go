6e9ad405a2305fc918d68e1520366dfa54da9dc7
lhdht <15229684090@163.com>
2024-04-17 10:11:05 +0800

    fix: 废弃grpc dial方法

--- grpc/entrance.go
+++ grpc/entrance.go
@@ -4,50 +4,12 @@ import (
 	"fmt"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 	"net"
-	"strings"
 )
-
-type ConfigEntity struct {
-	Deploy  bool   `json:"deploy"`  // current service deploy mode: local area network or not
-	Address string `json:"address"` // current service deploy address
-}
 
 type CoreEntity struct {
 	Server *grpc.Server
 	logger *zap.Logger
-}
-
-func (s *CoreEntity) Dial(endpoint []string, opt *ConfigEntity) *grpc.ClientConn {
-	var index int
-	length := len(endpoint)
-	if length == 0 {
-		s.logger.Warn("no service endpoint are available")
-		return nil
-	} else if length == 1 {
-		index = 0
-	} else {
-		// Load balancing algorithm that handles multiple nodes
-	}
-
-	address := endpoint[index]
-	if opt.Deploy {
-		srv := strings.Split(endpoint[index], ":")
-		cur := strings.Split(opt.Address, ":")
-
-		if srv[0] == cur[0] {
-			address = strings.Join([]string{"127.0.0.1", srv[1]}, ":")
-		}
-	}
-
-	conn, err := grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		s.logger.Error(fmt.Sprintf("the service endpoint is unavailable, error: %s", err.Error()))
-		return nil
-	}
-
-	return conn
 }
 
 func (s *CoreEntity) CreateServer(handle func(server *grpc.Server), address string) {
